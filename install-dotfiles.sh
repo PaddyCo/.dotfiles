8a873f91af4f1847bfb7d76ee33c4f1fd1068109
Patrik Riström <patrik@macbook.se>
2017-06-09 09:59:19 +0200

    Added global gitignore

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -22,3 +22,5 @@ ln -s $DIR/.vimrc ~/.config/nvim/init.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "vim-plug installed!"
 echo "neovim configured!"
+
+git config --global core.excludesfile $DIR/.gitignore_global
